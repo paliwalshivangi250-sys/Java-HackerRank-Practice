d1a7a1f2938832e70d729085890450e61c49b5aa
Shivangi Paliwal <paliwalshivangi250@gmail.com>
2026-04-29 04:17:47 +0530

    Added Java Stdin and Stdout I solution
    Added problem statement and platform details as comments.

---
+++ JavaStdinStdoutI.java
@@ -0,0 +1,19 @@
+// Problem: Java Stdin and Stdout I
+// Platform: HackerRank
+// Concepts Used: Scanner input
+
+public class Main{
+        public static void main(String args[]){
+       
+          Scanner sc= new Scanner(System.in);
+          
+          int n= sc.nextInt();
+          int p= sc.nextInt();
+          int q= sc.nextInt();
+     
+         System.out.println(n);
+          System.out.println(p);
+           System.out.println(q);
+
+
+}}
